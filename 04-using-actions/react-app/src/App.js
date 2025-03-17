5efa020efa658ea0fceed1f111565ef4449d7f30
Suresh Upadhya <suresh.upadhya@gmail.com>
2025-03-17 18:58:05 +0530

    react app

--- 04-using-actions/react-app/src/App.js
+++ 04-using-actions/react-app/src/App.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -8,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is a Prod Application and save to reload.
+          This is a production application
         </p>
         <a
           className="App-link"
